f5e794ca3ed6eebe9048a95711a3530c5b1f66d4
datomiriba <datomiribi@gmail.com>
2025-04-10 11:34:25 -0800

    commit

---
+++ day 002/homework/homework1.py
@@ -0,0 +1,3 @@
+# 1) ჩამოწერეთ and / or - ზე ყველა შესაძლო ვარიანტი და გვერდზე კომენტარის სახით მიუწერეთ რას გამოიტანს შედეგად.
+# true and false or false and true
+# true and true or false and true
---
+++ day 002/homework/homework3.py
@@ -0,0 +1,2 @@
+# 3) True or True and False or True and False რას გამოგვიტანს შედეგად 
+print(True or True and False or True and False )   #trues 
