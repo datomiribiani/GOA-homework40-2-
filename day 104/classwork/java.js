3d94b9191b4126ce947b5d3437a0ae77442ce62e
datomiribiani <156538029+datomiribiani@users.noreply.github.com>
2026-04-22 21:43:44 +0400

    commit

---
+++ day 104/classwork/java.js
@@ -0,0 +1,13 @@
+//  aris komentari
+
+// infs gamotana ---> console.log()
+
+// 1)კონსოლში გამოიტანეთ თქვენის სახელი , ასაკი , სიმაღლე(ათწილადი) და ერთი boolean ტიპის მნიშნელობა
+
+// console.log("dato")
+// console.log(16)
+// console.log(1.64)
+// console.log(true)
+
+// cvladis sheqmna 
+let name = "giorgi"
