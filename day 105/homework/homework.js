568e9321a697bd5004ff7b3560f0a3be3a4201bf
datomiribiani <156538029+datomiribiani@users.noreply.github.com>
2026-04-28 15:55:53 +0400

    commit

---
+++ day 105/homework/homework.js
@@ -0,0 +1,61 @@
+/*1)კომენტარის სახით ახსენით თუ რა არის .length 
+    .length ითვლის სტრინგების სიგრძეს
+
+2)let str = "hello";
+    console.log("hello".length) = 5
+
+დაბეჭდე რამდენი სიმბოლოა
+
+3)შექმენი ცვლადი სამივე keyword ის გამოყენებით,შენი დავალებაა დაბეჭდო სამივე ცვლადში შენახული სიმბოლოების ჯამი(გამოიყენე .length და მათემატიკური ოპერატორი)
+let name2 = "hellomn"
+var name1 =  "hello"
+const name = "hello"
+console.log(name2.length + name1.length + name.length)
+
+4)შექმენი ორი ერთში შეინახე სახელი მეორეში გვარი ,შემდეგ მოახდინე კონკატინაცია,გამოიყენე string interoplation და ბოლოს დაბეჭდე ამ მთლიანი სტრინგის სიგრძე
+let name1 = "dato"
+let name2 = "miribiani"
+console.log(`${name1 + name2}` )
+
+
+5)შექმენი ცვლადი let ის გამოყენებითდა შეინახე შენი სახელი,შემდეგ განაახლე ცვლადის მნიშნველბა და მიანიჭე მას "nika" დაბეჭდე საბოლოო შედეგი
+let nae = "dato"
+let nae = "niika" 
+console.log(nae)
+
+6)შექმენი ცვლადი var ის გამოყენებითდა შეინახე შენი სახელი,შემდეგ განაახლე ცვლადის მნიშნველბა და მიანიჭე მას "giorgi" დაბეჭდე საბოლოო შედეგი
+var nae1 = "dato"
+var nae1 = "giorgi"
+console.log(nae1)
+
+7)შექმენიც ცვლადი const ის გამოყენებით,შემდეგ შეცვალე მისი მნიშვნელობა ,რატომ არ შეცვალა მნიშვნელობა?
+const  name = "dato"
+const  name = "gigi"
+consti itimom ar sheicvala radgan ar sheilea shecvla tavdapirveli mnishneloba rac aq eg eqnebaa sul
+
+8)შექმენი ცვლადი let ის გამოყენებით და შიგნით არაფერი შეინახო,ამის შემდეგ სამჯერ განაახლე ამ ცვლადის მნიშნველობა,დაბეჭდე საბოლოო შედეგი
+let name = 
+let name = 15
+let name = "dd"
+let name = 23
+console.log(name)
+
+9)შექმენი ცვლადი var ის გამოყენებით და შიგნით არაფერი შეინახო,ამის შემდეგ სამჯერ განაახლე ამ ცვლადის მნიშნველობა,დაბეჭდე საბოლოო შედეგი
+var name = 
+vae name = 15434
+var name = "weed"
+var name = 221212
+console.log(name)
+
+10)შექმენი ცვლადი სამივე keyword ის გამოყენებით და შიგნით არაფერი შეიანხო,დაბეჭდე თითოეული მათგანი და ნახე რას მოგცემს თითოეული პასუხად
+let name =
+console.log(name) arafers dabejdavs
+
+var name =
+console.log(name) arc es arafers dabejdavs 
+const name =
+console.log(name) errors amoagdebs radgan amis shenaxva carieli ar sheileba
+*/
+
+
+
