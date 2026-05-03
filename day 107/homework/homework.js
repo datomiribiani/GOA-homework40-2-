d0f23ab651361a2689a3fc5d23087eec1d5bbc7e
datomiribiani <156538029+datomiribiani@users.noreply.github.com>
2026-05-03 09:58:07 +0400

    commit

--- day 107/homework/homework.js
+++ day 107/homework/homework.js
@@ -1,18 +1,29 @@
 /*1)კომენტარის სახით ახსენით თუ რა არის typeof და რაში გვეხმარება ის
-
+// typeof igebs monacemta tips  da gvexmareba romgavigot ra monacemta tipia
 2)შექმენი ცვლადი და შეინახე რაიმე რიცხვი,შენი დავალებაა გამოიტანო ამ ცვლადში შენახული ელემენტის მონაცემთა ტიპი
 
+let num1 =13
+console.log(typeof num1) number
+
 3)შექმენი ცვლადი და შეინახე რაიმე სტრინგი,შენი დავალებაა გამოიტანო ამ ცვლადში შენახული ელემენტის მონაცემთა ტიპი
-
+let str  = 'string
+console.log(typeof str) str
 4)შექმენი ცვლადი და შეინახე boolean ტიპის მონაცემი,შენი დავალებაა გამოიტანო ამ ცვლადში შენახული ელემენტის მონაცემთა ტიპი
-
+let boll1 = true
+console.log(boll1) 
 5)შექმენი ცვლადი და შეინახე undefined,შენი დავალებაა გამოიტანო ამ ცვლადში შენახული ელემენტის მონაცემთა ტიპი
-
+let name
+console.log(name)
 6)შექმენი ცვლადი სადაც შეინახავ სტრინგს,შემდეგ დაბეჭდე ამ ცვლადში შენახული ელემენტის ტიპი,შემდეგ განაახლე ამ ცვლადის მნიშვნელობა და შეინახე რაიმე რიცხვი,შემდეგ გამოიტანე განახლებული ცვლადს მონაცემთა ტიპი
+let name  = 'str'
+console.log(typeof name) str
+name = 25
+console.log(typeof name) number
 
 7)გაქვს ცვლადები:
 let name = "Goga";
 let age = 16;
+conosle.log(`My name is ${name} and I am ${age} years old`)
 
 შექმენი სტრინგი interpolation-ით:
 "My name is Goga and I am 16 years old"
@@ -21,7 +32,7 @@ let age = 16;
 
 let a = 5;
 let b = 3;
-
+console.log(`The sum of ${a} and ${b} is ${a + b}`)
 შექმენი სტრინგი:
 
 "The sum of 5 and 3 is 8"
@@ -29,31 +40,69 @@ let b = 3;
 გამოიყენე interpoaltion
 
 9)შექმენი ცვლადები შენს შესახებ,შენი დავალებაა რომ გამოიტანო ერთი წიანდადება შენს შესახებ სადაც გამოიყენებ ცვლადის მნიშვნელობებს,გამოიყენე interpoaltion
-
+let name = "dato"
+let age = 16
+let weight = 54.95
+let person = true
+console.log(`my name is ${name} and my age is ${age} , my weight is  ${ 54.95} an am person ${person}  )
 10)კომანტარის სახით ახსენით თუ რისი მეთოდებია --> .toUpperCase() , .toLowerCase() , .startsWith() , .trim() თითოეულ მათგანს მიუწერე თუ რომელი რაში გამოიყენება
-
+// .toUpperCase() didi asoebit bechdavs
+// .toLowerCase() patara asoebit bechdavs
+//.startsWith() iwyea tu ara frchxilebshi mocemuli stringit
+//.trim() gverdebidan ashorebs space ebs
 11)let str = "HELLO";
-
+console.log(str.toLowerCase())
 გადააკეთე პატარა ასოებად
-
 შედეგი: "hello"
 
 12)let name = "GoGa";
-
+console.log(name.toLowerCase())
 გააკეთე ისე, რომ მთლიანად პატარა ასოებით გამოვიდეს
 
 13)let word = "javascript";
+console.log(word.toUpperCase())
 
 გადააკეთე დიდ ასოებად
 შედეგი: "JAVASCRIPT"
 
 14)let city = "tbilisi";
-
+console.log(city.toUpperCase())
 დაბეჭდე დიდ ასოებში
 
 15)let str = "Hello world";
-
+console.log(str.startwith('Hello'))
 შეამოწმე იწყება თუ არა "Hello"-ით ზემოთ მოცემული წინადადება
-
 გამოიტანე true ან false
 */
+
+
+/*
+16)let email = "test@gmail.com";
+console.log(email.startwith('test'))
+შეამოწმე იწყება თუ არა "test"-ით
+
+17)let name = "giorgi"
+console.log(startwith('g'))
+შეამოწმე იწყება თუ არა ასო "g" ით
+
+18)let str = "   hello   ";
+console.log(str.trim())
+მოაშორე ზედმეტი space-ები დასაწყისსა და ბოლოში
+შედეგი: "hello"
+
+19)let username = "   goga123   ";
+მოაშორე ზედმეტი სფეისები და დაბეჭდე
+console.log(username.trim())
+20)let str = "   HeLLo WoRLd   ";
+console.log(str.trim().toLowerCase())
+
+დაბეჭდე იგივე სტრინგი ოღონდ მოაშორე სფეისები და გადაიყვანე პატარა ასოებში
+
+შედეგი: "hello world"
+
+21)let word = "   javascript      ";
+console.log(word.trim.startwith('java'))
+მოაშორე სფეისები და შემდეგ შეამოწმე იწყება თუ არა "java"-ით
+*/
+
+
