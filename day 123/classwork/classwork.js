3832a58b593a410d298832b3eb1adc0baa68a151
datomiribiani <156538029+datomiribiani@users.noreply.github.com>
2026-06-27 20:49:15 +0400

    commit

---
+++ day 123/classwork/classwork.js
@@ -0,0 +1,13 @@
+// 1)შექმენი ფუნცცია სახელად greet ,შიგნით შექმენი ცვლადი სახელად name და შეინახე რაიმე სახელი,შენი დავალებააა ფუნქციაში დააკონსოლლლოგო --> გამარჯობა ჩემი სახელია + name
+// გამოიძახე ფუნქცია რომ ნახო შედეგი,ახსენი რატომ გამოიტანა ეს შედეგი კომენტარის სახით 
+// შემდეგ ფუნქციის გარეთ დააკოლსოოლოგე name ცვლადი და ახსენი თუ რა მოხდა და რატომ მოხდა ასე
+function greet(){
+    const name = "name"
+    console.log("my name is" + name)
+}
+
+
+console.log(name)
+greet("dato")
+
+// daibejda my name isname radgan blockshi chavwere dakonsologeba pirveli da  magito daakonsologa eg
